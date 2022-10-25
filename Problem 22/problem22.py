d9e96e3ab911118488b6b31d5ba5bb21af1f5ac9
Joseph22Jct <66181369+Joseph22Jct@users.noreply.github.com>
2022-10-25 12:08:45 -0500

    Update problem22.py

--- Problem 22/problem22.py
+++ Problem 22/problem22.py
@@ -8,3 +8,33 @@
 # For example, given the set of words 'quick', 'brown', 'the', 'fox', and the string "thequickbrownfox", you should return ['the', 'quick', 'brown', 'fox'].
 
 # Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
+
+
+class Trie(object):
+    def addWord(self, word):
+        currentLetter = self.letterbase
+        for letter in word:
+            if(letter not in currentLetter):
+                currentLetter[letter] = {}
+            currentLetter = currentLetter[letter]
+        currentLetter["Finished"] = True ##Word is finished
+        pass
+
+    def __init__(self, set):
+        self.letterbase = {}
+        self.setbase = set
+        for word in set:
+            self.addWord(word)
+        print(str(self.letterbase))
+    
+
+def solve(set, word):
+    trie = Trie(set)
+    pass
+
+def main():
+    solve(["bed", "bath", "bedbath", "and", "beyond"], "bedbathandbeyond")
+    pass
+
+if __name__ == "__main__":
+    main()

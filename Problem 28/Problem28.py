6b0a6287986e2205cceb1fd62476d8d12a2fc645
Joseph22Jct <66181369+Joseph22Jct@users.noreply.github.com>
2022-10-31 13:14:28 -0500

    finished problem 28, updated problem 29

--- Problem 28/Problem28.py
+++ Problem 28/Problem28.py
@@ -4,7 +4,8 @@
 
 # Write an algorithm to justify text. Given a sequence of words and an integer line length k, return a list of strings which represents each line, fully justified.
 
-# More specifically, you should have as many words as possible in each line. There should be at least one space between each word. Pad extra spaces when necessary so that each line has exactly length k. Spaces should be distributed as equally as possible, with the extra spaces, if any, distributed starting from the left.
+# More specifically, you should have as many words as possible in each line. There should be at least one space between each word. Pad extra spaces when necessary 
+# so that each line has exactly length k. Spaces should be distributed as equally as possible, with the extra spaces, if any, distributed starting from the left.
 
 # If you can only fit one word on a line, then you should pad the right-hand side with spaces.
 
@@ -16,26 +17,46 @@
 # "fox  jumps  over", # 2 extra spaces distributed evenly
 # "the   lazy   dog"] # 4 extra spaces distributed evenly
 
+
 def solve(list, k):
     solution = []
     current = []
     for x in range(len(list)):
-        if(len(current)==0):
+        stri = ""
+        for w in current:
+            stri+=w
+        
+        if(len(stri+list[x])<= k - len(current) and x < len(list)-1):
             current.append(list[x])
         else:
-            stri = ""
-            for w in current:
-                stri+=w
-            if(len(stri+list[x])<= k - len(current)):
-                current.append(list[x])
+            stringToAdd = ""
+            stringToAdd+=current[0]
+            if(len(current)==1):
+                for add in range(k-len(current[0])):
+                    stringToAdd+=" "
             else:
-                stringToAdd = ""
-                stringToAdd+=current[0]
-                if(len(current)==1):
-                    for add in range(k-len(current[0])):
-                        stringToAdd+=" "
+                spacesToAdd = []
+                amountOfLetters = 0
+                for y in range(len(current)):
+                    amountOfLetters+= len(current[y])
+                spc = k-amountOfLetters
+                evenSpaces = int(spc/(len(current)-1))
+                remnants = spc%(len(current)-1)
+                for y in range(len(current)-1):
+                    spacesToAdd.append(evenSpaces)
 
+                for y in range(remnants):
+                    spacesToAdd[y] +=1
 
+                finalString = ""
+                for y in range(len(current)):
+                    finalString+=current[y]
+                    if (y< len(spacesToAdd)):
+                        for s in range(spacesToAdd[y]):
+                            finalString+=" "
+            solution.append(finalString)
+            current = []
+            current.append(list[x])
     return solution
 
 def main():
---
+++ Problem 29/problem29.py
@@ -0,0 +1,42 @@
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was asked by Amazon.
+
+# Run-length encoding is a fast and simple method of encoding strings. The basic idea is to represent repeated successive characters as a single count and character. 
+# For example, the string "AAAABBBCCDAA" would be encoded as "4A3B2C1D2A".
+
+# Implement run-length encoding and decoding. You can assume the string to be encoded have no digits and consists solely of alphabetic characters. 
+# You can assume the string to be decoded is valid.
+
+def decode(stng):
+    decoded = ""
+    
+
+    return decoded
+
+def encode(stng):
+    encoded = ""
+    cLetter = stng[0]
+    cNo = 0
+    for letter in stng:
+        if(cLetter==letter):
+            cNo+=1
+        else:
+            encoded +=str(cNo)
+            encoded +=cLetter
+            cLetter = letter
+            cNo=1
+    encoded +=str(cNo)
+    encoded +=cLetter
+    return encoded
+
+def main():
+    stng = "AAAABBBCCDAA"
+    stng = encode(stng)
+    print(stng)
+    stng = decode(stng)
+    print(stng)
+    pass
+
+if __name__ == "__main__":
+    main()
